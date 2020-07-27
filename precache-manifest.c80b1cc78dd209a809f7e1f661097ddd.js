3123b17724805028729057983dc5340ac2466c11
Aleksandr Mykhailov <ss.mykhailov@gmail.com>
2020-07-27 19:40:52 +0300

    Updates

---
+++ precache-manifest.c80b1cc78dd209a809f7e1f661097ddd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2422aa34a4aeb74d1bd2584dbdb6790a",
+    "url": "/react-hw-01-components/index.html"
+  },
+  {
+    "revision": "5a8cc4c67fd6531d0ee3",
+    "url": "/react-hw-01-components/static/css/main.c22af208.chunk.css"
+  },
+  {
+    "revision": "44da975a8b45e11aea6b",
+    "url": "/react-hw-01-components/static/js/2.92cdb186.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-hw-01-components/static/js/2.92cdb186.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a8cc4c67fd6531d0ee3",
+    "url": "/react-hw-01-components/static/js/main.94b0e767.chunk.js"
+  },
+  {
+    "revision": "6fe3cc52a8b0257f05d9",
+    "url": "/react-hw-01-components/static/js/runtime-main.459b7fff.js"
+  },
+  {
+    "revision": "4b7c63889e07f664b8b0e44362688af6",
+    "url": "/react-hw-01-components/static/media/images.4b7c6388.jpg"
+  }
+]);
